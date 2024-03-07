36d82c8fa308393cc52dffeb94d9a4c9ba1f104b
Moemen Gaballa <Moemen.gaballa@expertapps.com.sa>
2024-03-07 17:27:21 +0200

    init project and package to get load env file

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	fmt.Println("Port: ", portString)
+
+}
