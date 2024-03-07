468d67b6b4321d23960ecd40631a8d757ba2e821
Moemen Gaballa <Moemen.gaballa@expertapps.com.sa>
2024-03-07 17:42:12 +0200

    install server chi

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 	"log"
+	"net/http"
 	"os"
 )
 
@@ -15,6 +17,20 @@ func main() {
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server running on port %v", portString)
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	fmt.Println("Port: ", portString)
